dbe3d3641201dc55d9e5e625b54be4b2370e3b07
Eric <ebunta@users.noreply.github.com>
2015-01-20 19:10:38 -0500

    Dialog Box

---
+++ src/helloDialog/HelloDialog.java
@@ -0,0 +1,11 @@
+package helloDialog;
+
+import javax.swing.JOptionPane;
+
+public class HelloDialog 
+{
+	public static void main (String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Hello, world!");
+	}
+}
